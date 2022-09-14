25b9cc13a6011e535ea18a057197b956240d7c8c
Всеволод <woathatsweerzy@MacBook-Air-Vsevolod.local>
2022-09-14 13:11:07 +0300

    Загружаю проект

---
+++ 09.14/Program.cs
@@ -0,0 +1,15 @@
+﻿int sugarAmount = Convert.ToInt32(Console.ReadLine());
+
+/*for (int i = 0; i <= sugarAmount; i++)
+{
+    Console.WriteLine("В чае уже " + i + "Ложка сахара");
+}
+
+Console.ReadLine();*/
+int i = 1;
+while (i <= sugarAmount)
+{
+    Console.WriteLine("В чае уже " + i + "Ложка сахара");
+    i++;
+}
+Console.WriteLine(i);
